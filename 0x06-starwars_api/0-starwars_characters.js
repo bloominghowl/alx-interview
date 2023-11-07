512d7b78216cf83f7b87d8e5872f7812ee2e6751
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-11-07 12:15:43 +0000

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,27 @@
 #!/usr/bin/node
-/* Prints all Casts of Star Wars movie
-Read the README.md file for more info
-*/
 
 const request = require('request');
-const starWarsAPI = 'https://swapi-api.alx-tools.com/api/';
-const endPoint = 'films/';
-const movieID = process.argv[2].toString();
 
-request(starWarsAPI + endPoint + movieID, function (error, _, body) {
-  if (error) console.error(error);
-  const objects = JSON.parse(body);
-  const casts = objects.characters;
-  Printresult(casts);
-});
-
-/* reculsively and synchronously request for each character
-and prints out the casts */
-function Printresult (casts, counter = 0) {
-  request(casts[counter], function (error, _, body) {
-    if (error) console.error(error);
-    console.log(JSON.parse(body).name);
-    if (++counter < casts.length) {
-      Printresult(casts, counter++);
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
   });
-}
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
